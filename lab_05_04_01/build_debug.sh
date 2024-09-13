d3831623ed8dc89e47244099c18a36d7ea2bba1e
BelyiViktor <belyi_v_v@mail.ru>
2024-09-13 10:51:23 +0300

    Resolved conflicts in scripts for laboratory 5

--- lab_05_04_01/build_debug.sh
+++ lab_05_04_01/build_debug.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 # Компиляция и компоновка
-<<<<<<< HEAD
-<<<<<<<< HEAD:lab_02_04_01/build_debug.sh
-=======
-<<<<<<<< HEAD:lab_02_01_01/build_debug.sh
->>>>>>> main
-gcc -std=c99 -Werror -Wall -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -Wvla -c main.c -g3
-gcc -o app.exe main.o -lm
-========
 gcc -std=c99 -Werror -Wall -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -c ./*.c -g3 -D_POSIX_C_SOURCE=200112L
 gcc -o app.exe ./*.o -lm
->>>>>>>> main:lab_05_04_01/build_debug.sh
+
